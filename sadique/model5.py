ce16790661052bf1026806254bc10b4c2b82216c
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-08-08 14:02:15 +0530

    /

---
+++ sadique/model5.py
@@ -0,0 +1,26 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+model_name = "facebook/nllb-200-distilled-600M"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+translator = pipeline(
+    "translation",
+    model=model,
+    tokenizer=tokenizer,
+    src_lang="eng_Latn",   
+    tgt_lang="ory_Orya"    
+)
+
+print("English ➝ Odia Translator")
+print("Type 'exit' to quit.\n")
+
+while True:
+    english_text = input("Enter English text: ")
+    if english_text.lower() == "exit":
+        break
+
+    try:
+        result = translator(english_text)
+        print("Odia Translation:", result[0]["translation_text"], "\n")
+    except Exception as e:
+        print("Error:", str(e), "\n")
+
